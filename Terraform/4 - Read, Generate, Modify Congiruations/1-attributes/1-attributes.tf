a44b9135239a799899f7692d68b3110e8c172f61
Atin Gupta <atingupta2005@gmail.com>
2020-10-14 06:12:45 +0000

    -

--- Terraform/4 - Read, Generate, Modify Congiruations/1-attributes/1-attributes.tf
+++ Terraform/4 - Read, Generate, Modify Congiruations/1-attributes/1-attributes.tf
@@ -12,10 +12,10 @@ output "eip" {
   value = aws_eip.lb
 }
 
-resource "aws_s3_bucket" "atin_mys3" {
-  bucket = "kplabs-attribute-demo-001"
+resource "aws_s3_bucket" "atinbucket2020" {
+  bucket = "atinbucket2020"
 }
 
 output "mys3bucket" {
-  value = aws_s3_bucket.mys3
+  value = aws_s3_bucket.atinbucket2020
 }
