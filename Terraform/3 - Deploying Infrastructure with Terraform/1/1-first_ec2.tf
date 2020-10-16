8d20d7f477fec2d42ec1905bfb8688be47e1c2d3
Atin Gupta <atingupta2005@gmail.com>
2020-10-16 07:40:00 +0530

    -

--- Terraform/3 - Deploying Infrastructure with Terraform/1/1-first_ec2.tf
+++ Terraform/3 - Deploying Infrastructure with Terraform/1/1-first_ec2.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region     = "us-west-2"
-  access_key = ""
-  secret_key = ""
+  access_key = "AKIATV5HPRFNDNZDXPCA"
+  secret_key = "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"
 }
 
 # A resource block declares a resource of a given type ("aws_instance") 
